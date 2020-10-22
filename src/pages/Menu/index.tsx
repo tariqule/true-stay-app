1faa2d55c36637434d23fba546b50f7118ab2908
Tereza Konstari <teresa.konstari@gmail.com>
2020-10-21 23:56:26 -0400

    menu screen draft
    a draft of menu screen, questions:
    1. how to put user there?
    2. how to code icons like fb etc?
    3. go back -> where and same for all options?
    4.login screen -> show be attached there?

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,1 +1,83 @@
-/*comment*/
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from '../Login';
+
+function MenuScreen({ navigation }) {
+    return (
+    //user info goes here?
+
+      <View style={{ flex: 1, alignItems: 'left', justifyContent: 'left' }}>
+        <Button
+          onPress={() => navigation.navigate('Feed')}
+          title="FEED"
+        />
+        <Button
+          onPress={() => navigation.navigate('Explore')}
+          title="EXPLORE"
+        />
+        <Button
+          onPress={() => navigation.navigate('Messages')}
+          title="MESSAGES"
+        />
+        <Button
+          onPress={() => navigation.navigate('Notifications')}
+          title="NOTIFICATIONS"
+        />
+        <Button
+          onPress={() => navigation.navigate('Photos')}
+          title="PHOTOS"
+        />
+        <Button
+          onPress={() => navigation.navigate('Videos')}
+          title="VIDEOS"
+        />
+        <Button
+          onPress={() => navigation.navigate('Places')}
+          title="PLACES"
+        />
+        <Button
+          onPress={() => navigation.navigate('Settings')}
+          title="SETTINGS"
+        />
+        <Button
+          onPress={() => navigation.navigate('Search')}
+          title="SEARCH"
+        />
+      </View>
+    );
+  }
+
+ //code sample for going back -> should be with each option or should be 1 for all like return back home?
+ function GoBackScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'left', justifyContent: 'left' }}>
+      <Button onPress={() => navigation.goBack()} title="Home" />
+    </View>
+  );
+}
+
+//follow icons btn should be coded -> fb, instagram, twitter
+
+  const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="MENU" component={MenuScreen} />
+        <Drawer.Screen name="FEED" component={FeedScreen} />
+        <Drawer.Screen name="EXPLORE" component={ExploreScreen} />
+        <Drawer.Screen name="MESSAGES" component={MessageScreen} />
+        <Drawer.Screen name="NOTIFICATIONS" component={NotificationScreen} />
+        <Drawer.Screen name="PHOTOS" component={PhotoScreen} />
+        <Drawer.Screen name="VIDEOS" component={VideoScreen} />
+        <Drawer.Screen name="PLACES" component={PlaceScreen} />
+        <Drawer.Screen name="SETTINGS" component={SettingScreen} />
+        <Drawer.Screen name="SEARCH" component={SearchScreen} />
+        //icons
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
