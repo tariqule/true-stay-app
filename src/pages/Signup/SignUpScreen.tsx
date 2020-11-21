39ce26fa30022afc3c5358416f7a93f41993cfff
John Aneto <janeto@my.centennialcollege.ca>
2020-11-21 00:03:58 -0500

    Fixed visual bugs

--- src/pages/Signup/SignUpScreen.tsx
+++ src/pages/Signup/SignUpScreen.tsx
@@ -7,46 +7,54 @@ import {
   SafeAreaView,
   Button,
   TextInput,
+  ScrollView,
 } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 const screenWidth = Dimensions.get("screen").width;
+var screenHeight = Dimensions.get("screen").height;
+
+if (Dimensions.get("screen").height < 800) {
+  screenHeight = Dimensions.get("screen").height + 100;
+}
 
 export const SignUpScreen = () => {
   return (
-    <View style={styles.container}>
-      <SafeAreaView style={styles.header}>
-        <Text style={styles.heading}>TrueStay</Text>
-      </SafeAreaView>
-      <View style={styles.body}>
-        <View style={styles.main}>
-          <TextInput style={styles.input} placeholder="Username" />
-          <TextInput style={styles.input} placeholder="Password" />
-          <TextInput style={styles.input} placeholder="Confirm Password" />
-        </View>
-        <View style={styles.footer}>
-          <TouchableOpacity
-            style={styles.signUpButton}
-            onPress={() => console.log("Signed Up")}
-          >
-            <Text style={styles.buttonText}>Sign Up</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.fbButton}
-            onPress={() => console.log("FB button ")}
-          >
-            <Icon
-              style={styles.icon}
-              name="facebook"
-              size={20}
-              color="rgb(36, 160, 255)"
-            />
-            <Text style={styles.fbButtonText}>Sign Up with FaceBook</Text>
-          </TouchableOpacity>
+    <ScrollView style={styles.container}>
+      <View style={{ height: screenHeight }}>
+        <SafeAreaView style={styles.header}>
+          <Text style={styles.heading}>TrueStay</Text>
+        </SafeAreaView>
+        <View style={styles.body}>
+          <View style={styles.main}>
+            <TextInput style={styles.input} placeholder="Username" />
+            <TextInput style={styles.input} placeholder="Password" />
+            <TextInput style={styles.input} placeholder="Confirm Password" />
+          </View>
+          <View style={styles.footer}>
+            <TouchableOpacity
+              style={styles.signUpButton}
+              onPress={() => console.log("Signed Up")}
+            >
+              <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.fbButton}
+              onPress={() => console.log("FB button ")}
+            >
+              <Icon
+                style={styles.icon}
+                name="facebook"
+                size={20}
+                color="rgb(36, 160, 255)"
+              />
+              <Text style={styles.fbButtonText}>Sign Up with FaceBook</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -60,7 +68,6 @@ const styles = StyleSheet.create({
     backgroundColor: "rgb(187, 122, 68)",
     justifyContent: "flex-end",
     alignItems: "center",
-    padding: 20,
   },
   heading: {
     fontWeight: "bold",
@@ -82,7 +89,7 @@ const styles = StyleSheet.create({
   footer: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "flex-start",
+    justifyContent: "center",
   },
   input: {
     width: screenWidth - 30,
@@ -100,7 +107,7 @@ const styles = StyleSheet.create({
   fbButtonText: {
     color: "rgb(36, 160, 255)",
     textAlign: "center",
-    paddingRight: 50,
+    //paddingRight: 50,
     fontSize: 20,
   },
   signUpButton: {
@@ -123,6 +130,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   icon: {
-    width: 50,
+    width: 20,
   },
 });
